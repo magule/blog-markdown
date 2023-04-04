3b3c616fa8546191bf98135dd6c1d7b38c45a8c7
nygu <furkan@furkan-MacBook-Pro.local>
2023-04-04 23:34:43 +0300

    few style changes

--- components/PostPreview.tsx
+++ components/PostPreview.tsx
@@ -11,7 +11,7 @@ const PostPreview = (props: PostMetadata) => {
       <p className="text-sm text-slate-400">{props.date}</p>
 
       <Link href={`/posts/${props.slug}`}>
-        <h2 className=" text-sky-800 font-semibold hover:underline mb-4">{props.title}</h2>
+        <h2 className=" text-sky-800 font-semibold hover:underline mb-1">{props.title}</h2>
       </Link>
       <p className="text-slate-700">{props.subtitle}</p>
     </div>
